a0d939c1e72e8e808384f6cb9cad816d70d04088
Sowiarz <pavel.horubala@gmail.com>
2015-01-08 12:52:25 +0100

    Add LU decomposition project

---
+++ LU decomposition/src/lu/decomposition/LUDecomposition.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lu.decomposition;
+import Jama.*;
+
+/**
+ *
+ * @author Sowiarz
+ */
+public class LUDecomposition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Matrix exapmle;
+        double[][] temp = {{2.,4.,6.},{4.,10.,20.},{6.,20.,52.}};
+        Matrix example = new Matrix(temp);
+        
+        
+    }
+    
+}
